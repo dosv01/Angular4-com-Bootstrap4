cc5399cc8754521d8dbadda9f4294e90199c0bfd
dosv01 <danilo.oliveiras@gmail.com>
2017-08-23 00:36:26 -0300

    Curso finalizado
    finalizado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDrYKdd-9nvdLjIwt7VMN_En4LO-dKjA0",
+  authDomain: "angular4-2a901.firebaseapp.com",
+  databaseURL: "https://angular4-2a901.firebaseio.com",
+  storageBucket: "angular4-2a901.appspot.com",
+  messagingSenderId: "629529067580"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +22,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
